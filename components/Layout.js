7718484b082e214f6c9cd825b91ecf86e10fb7ca
Jake Lever <jake.lever@gmail.com>
2020-06-02 12:46:53 -0700

    Add analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,47 +1,63 @@
+import React, { Component } from 'react';
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar.js'
 import Topbar from '../components/Topbar.js'
 
-export default function Layout(props) {
-	return (
-		<div id="wrapper">
-			{/* Page Wrapper */}
-			<Head>
-				<title>{props.title}</title>
-			</Head>
+import { initGA, logPageView } from '../lib/analytics.js'
 
-			<Sidebar page={props.page} />
+class Layout extends Component {
+	
+	componentDidMount () {
+		if (!window.GA_INITIALIZED) {
+			initGA()
+			window.GA_INITIALIZED = true
+		}
+		logPageView()
+	}
+	
+	render() {
+		return (
+			<div id="wrapper">
+				{/* Page Wrapper */}
+				<Head>
+					<title>{this.props.title}</title>
+				</Head>
 
-			{/* Content Wrapper */}
-			<div id="content-wrapper" className="d-flex flex-column">
+				<Sidebar page={this.props.page} />
 
-				{/* Main Content */}
-				<div id="content">
+				{/* Content Wrapper */}
+				<div id="content-wrapper" className="d-flex flex-column">
 
-					<Topbar updateVirus={props.updateVirus} />
+					{/* Main Content */}
+					<div id="content">
 
-					{/* Begin Page Content */}
-					<div className="container-fluid">
-						{props.children}
-					</div>
-					{/* /.container-fluid */}
-					
-				</div>
-				{/* End of Main Content */}
-					
-					{/* Footer */}
-				<footer className="sticky-footer bg-white">
-					<div className="container my-auto">
-						<div className="copyright text-center my-auto">
-							<span>All data (where possible) are released under a Creative Commons Zero (CC0) licence.</span>
+						<Topbar updateVirus={this.props.updateVirus} />
+
+						{/* Begin Page Content */}
+						<div className="container-fluid">
+							{this.props.children}
 						</div>
+						{/* /.container-fluid */}
+						
 					</div>
-				</footer>
-				{/* End of Footer */}
+					{/* End of Main Content */}
+						
+						{/* Footer */}
+					<footer className="sticky-footer bg-white">
+						<div className="container my-auto">
+							<div className="copyright text-center my-auto">
+								<span>All data (where possible) are released under a Creative Commons Zero (CC0) licence.</span>
+							</div>
+						</div>
+					</footer>
+					{/* End of Footer */}
+
+				</div>
+				{/* End of Content Wrapper */}
 
 			</div>
-			{/* End of Content Wrapper */}
-
-		</div>
-	)
+		)
+	}
 }
+
+export default Layout
