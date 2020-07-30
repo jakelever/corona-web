e2f54ac4bee2d2727aa0f910312f4638baaf403c
Jake Lever <jake.lever@gmail.com>
2020-07-29 18:49:51 -0700

    First go at a more general search

--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
-import { AsyncTypeahead, Typeahead, Highlighter } from 'react-bootstrap-typeahead'
+import React, { Component, Fragment } from 'react';
+import { AsyncTypeahead, Typeahead, Highlighter, Menu, MenuItem, Hint, TypeaheadInputSingle } from 'react-bootstrap-typeahead'
 import { Badge } from 'react-bootstrap';
 import Router from 'next/router'
+
+import { groupBy } from 'lodash';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
@@ -12,6 +14,7 @@ export default class Search extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			input: '',
 			isLoading: false,
 			options: []
 			}
@@ -37,7 +40,10 @@ export default class Search extends Component {
 			const entity_name = selected[0].name
 			const entity_type = selected[0].type
 			
-			if (entity_type == 'topic' && entity_name in this.pageMapping) {
+			if (entity_type == 'search') {
+				url = "/search?q=" + entity_name
+				Router.push("/search",url)
+			} else if (entity_type == 'topic' && entity_name in this.pageMapping) {
 				const url = "/" + this.pageMapping[entity_name]
 				Router.push("/[id]",url)
 			} else if (entity_type == 'Paper') {
@@ -56,51 +62,88 @@ export default class Search extends Component {
 				Router.push("/entity/[...typename]",url)
 			}
 		}
+		//console.log('change!')
 	}
 	
 	render() {
 		
-		var data = [
-		{name:'remdesivir',type:'drug'},
-		{name:'lopinavir',type:'drug'},
-		{name:'darunavir',type:'drug'},
-		{name:'Spain',type:'place'},
-		];
-		
 		const renderSearchRow = (option, props, index) => {
 			return [
-			  <Highlighter key="name" search={props.text}>
+			<Highlighter key="name" search={props.text}>
 				{option.name}
-			  </Highlighter>,
-			  <div key="type" style={{float:"right"}}>
+			</Highlighter>,
+			<div key="type" style={{float:"right"}}>
 				<Badge variant="secondary">
-				  {option.type}
+					{option.type}
 				</Badge>
-			  </div>,
+			</div>,
 			];
-		  }
+		}
 		  
-		  
-		/* 
+		/*const optionsWithGeneralSearch = [
+		{'name':'Search for papers containing "Glasgow"', 'type':'Moo'},
+		...this.state.options
+		]*/
 		
-					  
-					  onSearch={query => this.setState({options: [{name:'moomoo',type:'drug'}]})}
-					  
-					  */
+		/*const renderMenu = (results, menuProps, state) => {
+			let index = 0;
+			const types = groupBy(results, 'type');
+			const items = Object.keys(types).sort().map((type) => (
+				<Fragment key={type}>
+					{index !== 0 && <Menu.Divider />}
+					<Menu.Header>{type}</Menu.Header>
+					{types[type].map((i) => {
+						const item =
+						<MenuItem key={index} option={i} position={index}>
+							<Highlighter search={state.text}>
+							{i.name}
+							</Highlighter>
+						</MenuItem>;
+
+						index += 1;
+						return item;
+					})}
+				</Fragment>
+			))
+			
+			return <Menu {...menuProps}>{items}</Menu>
+		}*/
 		
+		const renderMenu = (results, menuProps, state) => {
+			const generalSearchOption = {'name':state.text, 'type':'search'}
+			const generalSearch = <MenuItem option={generalSearchOption} position={0}>Search for papers containing {'"'+state.text+'"'}</MenuItem>
+			
+			const renderedResults = results.map( (option,i) => <MenuItem key={i} option={option} position={i+1}>{renderSearchRow(option,state,i)}</MenuItem> )
+			
+			return <Menu {...menuProps}>{generalSearch}{renderedResults}</Menu>
+		}
+		
+		// renderMenu={renderMenu}
+		// options={optionsWithGeneralSearch}
+		// onKeyDown={event => console.log(event.key)}
 		return (
 		<div>
 			<div className="input-group">
 			
 				<AsyncTypeahead
 					isLoading={this.state.isLoading}
-					id="basic-typeahead-example"
-					renderMenuItemChildren={renderSearchRow}
+					id="typeahead"
 					labelKey="name"
 					onSearch={this.search}
 					onChange={this.onChange}
+					renderMenu={renderMenu}
+					onInputChange={(txt,event) => this.setState({input:txt})}
 					options={this.state.options}
 					placeholder="Search for a drug, gene, location, paper, etc"
+					onKeyDown={event => {
+						if (event.key == 'Enter' && this.state.input) {
+							//console.log(this.state.input)
+							
+							const url = "/search?q=" + this.state.input
+							Router.push("/search",url)
+						}
+					}}
+					
 				  />
 				
 				<div className="input-group-append">
---
+++ pages/search.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import Link from 'next/link'
+
+import Layout from '../components/Layout.js'
+import CustomTable from '../components/CustomTable.js'
+import pages from '../lib/pages.json'
+import { searchTitleAbstracts } from '../lib/db-search.js'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { Bar } from 'react-chartjs-2';
+
+export async function getServerSideProps(context) {
+	//console.log(context)
+	var tabledata = []
+	const query = 'q' in context.query ? context.query.q : ''
+	if (query) {
+		tabledata = await searchTitleAbstracts(query)
+	}
+	
+	return {
+		props: {
+			fallback_complete: true,
+			query,
+			tabledata
+		}
+	}
+}
+
+export default class Page extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			viruses: ['MERS-CoV','SARS-CoV','SARS-CoV-2'],
+			windowWidth: null
+			}
+		
+		this.barClick = this.barClick.bind(this);
+		this.updateViruses = this.updateViruses.bind(this);
+		this.filterForVirus = this.filterForVirus.bind(this);
+		this.downloadJSON = this.downloadJSON.bind(this);
+		this.handleResize = this.handleResize.bind(this);
+	}
+	
+	handleResize(windowWidth) {
+		this.setState({windowWidth:windowWidth})
+	}
+
+	barClick(elems) {
+		console.log(elems)
+	}
+	
+	updateViruses(viruses) {
+		this.setState({viruses: viruses})
+	}
+	
+	filterForVirus(row) {
+		if (this.state.viruses.length == 0)
+			return true;
+		
+		var row_viruses = row['entities'].filter(e => e['type'] == 'Virus').map(e => e['name']);
+		var overlap = this.state.viruses.filter(v => row_viruses.includes(v))
+		return overlap.length > 0
+	}
+	
+	// https://codepen.io/Jacqueline34/pen/pyVoWr
+	downloadJSON(event, data) {
+		const link = document.createElement('a')
+
+		var json = JSON.stringify(data)
+		const filename = 'search_results.json'
+		
+		json = `data:text/json;charset=utf-8,${json}`
+
+		link.setAttribute('href', encodeURI(json))
+		link.setAttribute('download', filename)
+		link.click()
+		
+		event.preventDefault()
+	}
+	
+
+	render() {
+		if(!this.props.fallback_complete)
+			return <Layout loading={true}></Layout>
+		if (!this.props.tabledata)
+			return <Layout error404={true}></Layout>
+		
+		
+		var columns = [
+				{ "header":"Virus", "selector":"entities:Virus", "hide":"md", grow:1 },
+				{ "header":"Journal", "selector":"journal", "hide":"md", grow:1 },
+				{ "header":"Date", "selector":"publish_date", "hide":"md", grow:1 },
+				{ "header":"Title", "selector":"title", linkInternal: true, grow: 4 }
+			]
+				
+		const filteredData = this.props.tabledata.filter(row => this.filterForVirus(row));
+		const filteredDataNoAltmetric = filteredData.map( row => {
+			var newRow = {}
+			Object.keys(row).forEach( k => {
+				if (!k.includes('altmetric'))
+					newRow[k] = row[k]
+			})
+			return newRow
+		})
+		
+		const table = <CustomTable columns={columns} data={filteredData} />
+
+		return (
+			<Layout title="Search Results" page="/search" updateViruses={this.updateViruses} showVirusSelector handleResize={this.handleResize}>
+		
+				{/* Page Heading */}
+				<div className="d-sm-flex align-items-center justify-content-between mb-4">
+					<h1 className="h3 mb-0 text-gray-800">Search Results for {'"'+this.props.query+'"'}</h1>
+					<a href="#" onClick={event => this.downloadJSON(event,filteredDataNoAltmetric)} className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+						<span className="text-white-50"><FontAwesomeIcon icon={faDownload} size="sm" /></span> Download Data
+					</a>
+				</div>
+				
+
+				<div className="card shadow mb-4">
+					<div className="card-header py-3">
+						<h6 className="m-0 font-weight-bold text-primary">Published and Preprint Papers</h6>
+					</div>
+					<div className="card-body">
+						<div className="table-responsive">
+								
+								{table}
+								
+						</div>
+					</div>
+				</div>
+				
+
+			</Layout>
+		)
+	}
+}
+
