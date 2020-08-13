dfb67b0284acf544e0c2c4c100799486e3c3c67d
Jake Lever <jake.lever@gmail.com>
2020-08-12 18:16:19 -0700

    Reduce table padding and fix flag modal not working in table

--- components/CustomTable.js
+++ components/CustomTable.js
@@ -26,213 +26,7 @@ const shortMonths = ["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","O
 
 
 
-function getColumnMetadata(column) {
-	var metadata
-	
-	const nonEntities = ['journal','title','publish_timestamp','flagandlink','altmetric_score_1day','altmetric_score']
-	
-	const isEntity = !nonEntities.includes(column)
-	
-	if (column == 'flagandlink') {
-		const renderButtonColumn = row => {
-			const flag = <a className="flagtime" href="#" onClick={event => {this.showFlagModal(row); event.preventDefault()}}><FontAwesomeIcon icon={faExclamationTriangle} size="lg" /></a>
-			const linkDoc = <a className="flagtime" href={row.url} target="_blank"><FontAwesomeIcon icon={faExternalLinkAlt} size="lg" /></a>
-			
-			return <div className="tour-tablebuttons"><p>{linkDoc}</p><p>{flag}</p></div>
-		}
-		
-		metadata = {
-				id: 'flagandlink',
-				cell: renderButtonColumn,
-				ignoreRowClick: true,
-				allowOverflow: true,
-				button: true,
-				hide: "sm",
-				style: {
-				  padding: '14px'
-				},
-				grow: 1
-		}
-	} else if (column == 'altmetric_score') {
-		const renderAltmetricBadge = row => {
-			if (row.altmetric_id == -1)
-				return ''
-			
-			const badgeURL = "https://badges.altmetric.com/?size=80&score=" + row.altmetric_score + "&types=" + row.altmetric_badgetype
-			const detailsURL = "http://www.altmetric.com/details.php?citation_id=" + row.altmetric_id
-			const img = <img src={badgeURL} />
-			return <a className="tour-altmetric" href={detailsURL} target="_blank" alt={"Altmetric score of " + row.altmetric_score}>{img}</a>
-		}
-			
-		metadata = {
-				id: 'altmetric',
-				name: 'Altmetric',
-				selector: 'altmetric_score',
-				cell: renderAltmetricBadge,
-				sortable: true,
-				allowOverflow: true,
-				button: true,
-				grow: 1
-			}
-	} else if (column == 'altmetric_score_1day') {
-		const renderAltmetricScore1Day = row => {
-			if (row.altmetric_id == -1)
-				return ''
-			
-			const badgeURL = "https://badges.altmetric.com/?size=64&score=" + row.altmetric_score_1day + "&types=" + row.altmetric_badgetype
-			const detailsURL = "http://www.altmetric.com/details.php?citation_id=" + row.altmetric_id
-			const img = <img src={badgeURL} />
-			return <a href={detailsURL} target="_blank" alt={"Altmetric 1 day score of " + row.altmetric_score_1day}>{img}</a>
-		}
-		
-		metadata = {
-				id: 'altmetric_1day',
-				name: 'Altmetric 1 Day',
-				selector: 'altmetric_score_1day',
-				cell: renderAltmetricScore1Day,
-				sortable: true,
-				allowOverflow: true,
-				button: true,
-				grow: 1
-			}
-	} else if (isEntity) {
-		var entity_type = column
-		
-		var pageMapping = {}
-		if (entity_type == 'topic') {
-			pages.forEach(p => {pageMapping[p.name] = p.page})
-		}
-		
-		metadata = {
-			id: entity_type,
-			name: column in niceNames ? niceNames[column] : column,
-			sortable: false,
-			//width: '10%',
-			style: {
-			  fontSize: '16px',
-			  padding: '14px'
-			},
-			wrap: true,
-			cell: row => {
-				var entities; 
-				if (entity_type == 'topic') {
-					entities = row.entities.filter( e => e.type==entity_type ).map( (e,i) => <Link key={'entitylink_'+i} href="/[id]" as={`/${pageMapping[e.name]}`}><a key={'entity_'+i}>{e.name}</a></Link> )
-				} else if (entity_type == 'Virus' || entity_type == 'articletype') {
-					entities = row.entities.filter( e => e.type==entity_type ).map( (e,i) => e.name )
-				} else {
-					entities = row.entities.filter( e => e.type==entity_type ).map( (e,i) => <Link key={'entitylink_'+i} href={"/entity/[...typename]"} as={"/entity/"+e.type+"/"+e.name}><a key={'entity_'+i}>{e.name}</a></Link> )
-				}
-				
-				
-				var combined = entities.length > 0 ? entities.reduce((prev, curr) => [prev, ', ', curr]) : ''
-				
-				return <div>{combined}</div>
-			},
-			grow: 2
-		}
-	} else if (column == 'publish_timestamp') {
-		metadata = {
-			id: column,
-			name: 'Date',
-			selector: "publish_year",
-			sortable: true,
-			style: {
-			  fontSize: '16px',
-			  padding: '14px'
-			},
-			wrap: false, 
-			sortFunction: (rowA, rowB) => {
-				const dateA = (rowA.publish_year ? rowA.publish_year.toString().padStart(4,'0') : '0000') + (rowA.publish_month ? rowA.publish_month.toString().padStart(2,'0') : '00') + (rowA.publish_day ? rowA.publish_day.toString().padStart(2,'0') : '00')
-				const dateB = (rowB.publish_year ? rowB.publish_year.toString().padStart(4,'0') : '0000') + (rowB.publish_month ? rowB.publish_month.toString().padStart(2,'0') : '00') + (rowB.publish_day ? rowB.publish_day.toString().padStart(2,'0') : '00')
-				
-				return parseInt(dateA) - parseInt(dateB)
-			},
-			cell: row => { 
-				/*if (row.publish_year && row.publish_month && row.publish_day) {
-					return row.publish_year.toString() + "-" + row.publish_month.toString().padStart(2,'0') + "-" + row.publish_day.toString().padStart(2,'0')
-				} else if (row.publish_year && row.publish_month) {
-					return row.publish_year.toString() + "-" + row.publish_month.toString().padStart(2,'0')
-				} else if (row.publish_year) {
-					return row.publish_year.toString()
-				} else {
-					return ""
-				}*/
-				
-				if (row.publish_year && row.publish_month && row.publish_day) {
-					return row.publish_day.toString() + ' ' + shortMonths[row.publish_month] + ' ' + row.publish_year.toString()
-				} else if (row.publish_year && row.publish_month) {
-					return shortMonths[row.publish_month] + ' ' + row.publish_year.toString()
-				} else if (row.publish_year) {
-					return row.publish_year.toString()
-				} else {
-					return ""
-				}
-			}
-		}
-	} else {
-		metadata = {
-			id: column,
-			name: column in niceNames ? niceNames[column] : column,
-			selector: column,
-			sortable: true,
-			style: {
-			  fontSize: '16px',
-			  padding: '14px'
-			},
-			wrap: true,
-			grow: 1
-		}
-	}
-	
-	/*if (column.grow) {
-		metadata.grow = column.grow
-	}
-	if (column.minWidth) {
-		metadata.minWidth = column.minWidth
-	}
-	if (column.maxWidth) {
-		metadata.maxWidth = column.maxWidth
-	}
-	if (column.width) {
-		metadata.width = column.width
-	}
-	
-	if (column.hide) {
-		metadata.hide = column.hide
-	}
-	
-	if (column.linkExternal) {
-		metadata.cell = row => <a href={row.url} target="_blank">{row[column.selector]}</a>
-	}*/
-	
-	if (column == 'title') {
-		const linkFunction = row => {
-			if (row.doi) {
-				return <Link href={"/doc/[...identifiers]"} as={"/doc/doi/"+row.doi} prefetch={false}><a>{row[column]}</a></Link>
-			} else if (row.pubmed_id) {
-				return <Link href={"/doc/[...identifiers]"} as={"/doc/pubmed_id/"+row.pubmed_id} prefetch={false}><a>{row[column]}</a></Link>
-			} else if (row.cord_uid) {
-				return <Link href={"/doc/[...identifiers]"} as={"/doc/cord_uid/"+row.cord_uid} prefetch={false}><a>{row[column]}</a></Link>
-			} else {
-				return row[column]
-			}
-		}
-		
-		metadata.cell = linkFunction
-		metadata.grow = 4
-	}
-	
-	if (column == 'Virus') {
-		metadata.grow = 1
-		//metadata.hide = "md"
-	}
-	
-	if (column == 'journal' || column == 'publish_timestamp') {
-		//metadata.hide = "md"
-	}
-	
-	return metadata
-}
+
 
 const customStyles = {
 		  headRow: {
@@ -278,6 +72,7 @@ export default class CustomTable extends Component {
 		this.stopHidingColumn = this.stopHidingColumn.bind(this)
 		this.updateFilters = this.updateFilters.bind(this)
 		this.updateRanges = this.updateRanges.bind(this)
+		this.getColumnMetadata = this.getColumnMetadata.bind(this)
 	}
 	
 	closeFlagModal() {
@@ -319,6 +114,224 @@ export default class CustomTable extends Component {
 	
 	updateRanges(ranges) {
 		this.setState({ranges:ranges})
+	}
+	
+	getColumnMetadata(column) {
+		var metadata
+		
+		const nonEntities = ['journal','title','publish_timestamp','flagandlink','altmetric_score_1day','altmetric_score']
+		
+		const isEntity = !nonEntities.includes(column)
+		
+		if (column == 'flagandlink') {
+			const renderButtonColumn = row => {
+				const flag = <a className="flagtime" href="#" onClick={event => {this.showFlagModal(row); event.preventDefault()}}><FontAwesomeIcon icon={faExclamationTriangle} size="lg" /></a>
+				const linkDoc = <a className="flagtime" href={row.url} target="_blank"><FontAwesomeIcon icon={faExternalLinkAlt} size="lg" /></a>
+				
+				return <div className="tour-tablebuttons"><p>{linkDoc}</p><p>{flag}</p></div>
+			}
+			
+			metadata = {
+					id: 'flagandlink',
+					cell: renderButtonColumn,
+					ignoreRowClick: true,
+					allowOverflow: true,
+					button: true,
+					hide: "sm",
+					style: {
+					  paddingTop: '14px',
+					  paddingBottom: '14px'
+					},
+					grow: 1
+			}
+		} else if (column == 'altmetric_score') {
+			const renderAltmetricBadge = row => {
+				if (row.altmetric_id == -1)
+					return ''
+				
+				const badgeURL = "https://badges.altmetric.com/?size=80&score=" + row.altmetric_score + "&types=" + row.altmetric_badgetype
+				const detailsURL = "http://www.altmetric.com/details.php?citation_id=" + row.altmetric_id
+				const img = <img src={badgeURL} />
+				return <a className="tour-altmetric" href={detailsURL} target="_blank" alt={"Altmetric score of " + row.altmetric_score}>{img}</a>
+			}
+				
+			metadata = {
+					id: 'altmetric',
+					name: 'Altmetric',
+					selector: 'altmetric_score',
+					cell: renderAltmetricBadge,
+					sortable: true,
+					allowOverflow: true,
+					button: true,
+					grow: 1
+				}
+		} else if (column == 'altmetric_score_1day') {
+			const renderAltmetricScore1Day = row => {
+				if (row.altmetric_id == -1)
+					return ''
+				
+				const badgeURL = "https://badges.altmetric.com/?size=64&score=" + row.altmetric_score_1day + "&types=" + row.altmetric_badgetype
+				const detailsURL = "http://www.altmetric.com/details.php?citation_id=" + row.altmetric_id
+				const img = <img src={badgeURL} />
+				return <a href={detailsURL} target="_blank" alt={"Altmetric 1 day score of " + row.altmetric_score_1day}>{img}</a>
+			}
+			
+			metadata = {
+					id: 'altmetric_1day',
+					name: 'Altmetric 1 Day',
+					selector: 'altmetric_score_1day',
+					cell: renderAltmetricScore1Day,
+					sortable: true,
+					allowOverflow: true,
+					button: true,
+					grow: 1
+				}
+		} else if (isEntity) {
+			var entity_type = column
+			
+			var pageMapping = {}
+			if (entity_type == 'topic') {
+				pages.forEach(p => {pageMapping[p.name] = p.page})
+			}
+			
+			metadata = {
+				id: entity_type,
+				name: column in niceNames ? niceNames[column] : column,
+				sortable: false,
+				//width: '10%',
+				style: {
+				  fontSize: '16px',
+				  paddingLeft: '8px',
+				  paddingRight: '8px',
+				  paddingTop: '16px',
+				  paddingBottom: '16px'
+				},
+				wrap: true,
+				cell: row => {
+					var entities; 
+					if (entity_type == 'topic') {
+						entities = row.entities.filter( e => e.type==entity_type ).map( (e,i) => <Link key={'entitylink_'+i} href="/[id]" as={`/${pageMapping[e.name]}`}><a key={'entity_'+i}>{e.name}</a></Link> )
+					} else if (entity_type == 'Virus' || entity_type == 'articletype') {
+						entities = row.entities.filter( e => e.type==entity_type ).map( (e,i) => e.name )
+					} else {
+						entities = row.entities.filter( e => e.type==entity_type ).map( (e,i) => <Link key={'entitylink_'+i} href={"/entity/[...typename]"} as={"/entity/"+e.type+"/"+e.name}><a key={'entity_'+i}>{e.name}</a></Link> )
+					}
+					
+					
+					var combined = entities.length > 0 ? entities.reduce((prev, curr) => [prev, ', ', curr]) : ''
+					
+					return <div>{combined}</div>
+				},
+				grow: 2
+			}
+		} else if (column == 'publish_timestamp') {
+			metadata = {
+				id: column,
+				name: 'Date',
+				selector: "publish_year",
+				sortable: true,
+				style: {
+				  fontSize: '16px',
+				  paddingLeft: '8px',
+				  paddingRight: '8px',
+				  paddingTop: '16px',
+				  paddingBottom: '16px'
+				},
+				wrap: false, 
+				sortFunction: (rowA, rowB) => {
+					const dateA = (rowA.publish_year ? rowA.publish_year.toString().padStart(4,'0') : '0000') + (rowA.publish_month ? rowA.publish_month.toString().padStart(2,'0') : '00') + (rowA.publish_day ? rowA.publish_day.toString().padStart(2,'0') : '00')
+					const dateB = (rowB.publish_year ? rowB.publish_year.toString().padStart(4,'0') : '0000') + (rowB.publish_month ? rowB.publish_month.toString().padStart(2,'0') : '00') + (rowB.publish_day ? rowB.publish_day.toString().padStart(2,'0') : '00')
+					
+					return parseInt(dateA) - parseInt(dateB)
+				},
+				cell: row => { 
+					/*if (row.publish_year && row.publish_month && row.publish_day) {
+						return row.publish_year.toString() + "-" + row.publish_month.toString().padStart(2,'0') + "-" + row.publish_day.toString().padStart(2,'0')
+					} else if (row.publish_year && row.publish_month) {
+						return row.publish_year.toString() + "-" + row.publish_month.toString().padStart(2,'0')
+					} else if (row.publish_year) {
+						return row.publish_year.toString()
+					} else {
+						return ""
+					}*/
+					
+					if (row.publish_year && row.publish_month && row.publish_day) {
+						return row.publish_day.toString() + ' ' + shortMonths[row.publish_month] + ' ' + row.publish_year.toString()
+					} else if (row.publish_year && row.publish_month) {
+						return shortMonths[row.publish_month] + ' ' + row.publish_year.toString()
+					} else if (row.publish_year) {
+						return row.publish_year.toString()
+					} else {
+						return ""
+					}
+				}
+			}
+		} else {
+			metadata = {
+				id: column,
+				name: column in niceNames ? niceNames[column] : column,
+				selector: column,
+				sortable: true,
+				style: {
+				  fontSize: '16px',
+				  paddingLeft: '8px',
+				  paddingRight: '8px',
+				  paddingTop: '16px',
+				  paddingBottom: '16px'
+				},
+				wrap: true,
+				grow: 1
+			}
+		}
+		
+		/*if (column.grow) {
+			metadata.grow = column.grow
+		}
+		if (column.minWidth) {
+			metadata.minWidth = column.minWidth
+		}
+		if (column.maxWidth) {
+			metadata.maxWidth = column.maxWidth
+		}
+		if (column.width) {
+			metadata.width = column.width
+		}
+		
+		if (column.hide) {
+			metadata.hide = column.hide
+		}
+		
+		if (column.linkExternal) {
+			metadata.cell = row => <a href={row.url} target="_blank">{row[column.selector]}</a>
+		}*/
+		
+		if (column == 'title') {
+			const linkFunction = row => {
+				if (row.doi) {
+					return <Link href={"/doc/[...identifiers]"} as={"/doc/doi/"+row.doi} prefetch={false}><a>{row[column]}</a></Link>
+				} else if (row.pubmed_id) {
+					return <Link href={"/doc/[...identifiers]"} as={"/doc/pubmed_id/"+row.pubmed_id} prefetch={false}><a>{row[column]}</a></Link>
+				} else if (row.cord_uid) {
+					return <Link href={"/doc/[...identifiers]"} as={"/doc/cord_uid/"+row.cord_uid} prefetch={false}><a>{row[column]}</a></Link>
+				} else {
+					return row[column]
+				}
+			}
+			
+			metadata.cell = linkFunction
+			metadata.grow = 4
+		}
+		
+		if (column == 'Virus') {
+			metadata.grow = 1
+			//metadata.hide = "md"
+		}
+		
+		if (column == 'journal' || column == 'publish_timestamp') {
+			//metadata.hide = "md"
+		}
+		
+		return metadata
 	}
 	
 	/*componentDidUpdate(prevProps) {
@@ -367,7 +380,7 @@ export default class CustomTable extends Component {
 		
 		
 
-		var columnsWithFormating = columnsToShow.map( column => getColumnMetadata(column) )
+		var columnsWithFormating = columnsToShow.map( column => this.getColumnMetadata(column) )
 		
 		const optionalProps = ['paginationPerPage','paginationRowsPerPageOptions']
 		var extraProps = {}
